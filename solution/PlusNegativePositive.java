3e28cde7f2a3f801589c7bb4d0f9f6a312ad4cf1
system-out-print-jiyeon <67897652+system-out-print-jiyeon@users.noreply.github.com>
2022-03-11 10:58:57 +0900

    음양더하기

--- solution/PlusNegativePositive.java
+++ solution/PlusNegativePositive.java
@@ -1,19 +1,17 @@
-package kr.co.study.algorithm;
-
 public class Plusminus {
 
 		public static void main(String[] args) {
 			int[] absolutes = {4,7,12};
 			boolean[] signs = {true,false,true};
 			
-			PMSolution solution = new PMSolution();
+			Solution solution = new Solution();
 			
 			System.out.println(solution.solution(absolutes, signs));
 		} 
 
 }
 
-class PMSolution{
+class Solution{
 	  public int solution(int[] absolutes, boolean[] signs) {
 	        int answer = 0;
 	        
